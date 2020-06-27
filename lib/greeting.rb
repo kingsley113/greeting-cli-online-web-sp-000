62718e2893def2eea3bffcf4abaca0c5c405cb49
Cameron Kingsley <cameron.kingsley13@gmail.com>
2020-06-27 00:58:56 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,1 +1,10 @@
 # code the #greeting method here!
+
+puts "What is your name?"
+user_name = gets.strip
+
+def greeting(name)
+  puts "Hello #{name}. It's nice to meet you."
+end
+
+greeting(user_name)
